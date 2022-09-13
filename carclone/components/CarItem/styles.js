5e2d1820286a6911cf4e7578ad5b2a893dcc7de5
Daddy91 <daddymosanda@live.fr>
2022-09-13 08:41:04 +0200

    first commit

---
+++ carclone/components/CarItem/styles.js
@@ -0,0 +1,50 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const styles = StyleSheet.create({
+    carcontainer:{
+        width:'100%',
+        height:'100%',
+        height: Dimensions.get('window').height,
+    
+      },
+      titles:{
+        marginTop:'30%',
+        width:'100%',
+        alignItems:'center',
+    
+      },
+      title:{
+        fontSize: 40,
+        fontWeight: '500',
+    
+      },
+      subtitle:{
+        fontSize: 16,
+        color: '#5c5e62',
+    
+      },
+      subtitleCTA:{
+        //fontSize: 16,
+        //color: '#5c5e66',
+        textDecorationLine:'underline',
+      },
+      image:{
+        width:'100%',
+        height:'100%',
+        resizeMode: 'cover',
+        position:'absolute',
+      },
+      buttonContainer:{
+        position:'absolute',
+        bottom: 50,
+        width: '100%',
+        height:'10%'
+        
+        
+
+      }
+      
+
+});
+
+export default styles;
